14fad4543487abc51af99a2294368a8c23126845
Derek Splunk <ddesjardins@splunk.com>
2021-12-29 15:04:08 -0700

    removed manual instrumentation

--- shop/src/main/java/com/shabushabu/javashop/shop/controllers/HomeController.java
+++ shop/src/main/java/com/shabushabu/javashop/shop/controllers/HomeController.java
@@ -9,23 +9,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.context.Scope;
-//import io.opentelemetry.api.trace.attributes.SemanticAttributes;
 //import io.opentelemetry.extension.auto.annotations.WithSpan;
 
 import com.shabushabu.javashop.shop.services.ProductService;
 
 @Controller
 public class HomeController {
-	private static final Tracer s_tracer =
-			GlobalOpenTelemetry.getTracer("javasshop.tracer");
+	//private static final Tracer s_tracer = GlobalOpenTelemetry.getTracer("javasshop.tracer");
 
     @Autowired
     private ProductService productService;
 
+    
+    /*
    @RequestMapping(value="/", method = RequestMethod.GET)
    public String usingRequestParam(Model model, @RequestParam(value="name", required=true) String thename, @RequestParam(value="color", required=true) String thecolor) {
 
@@ -50,31 +46,26 @@ public class HomeController {
 
      return "index";
    } 
+   */
+    
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String usingRequestParam(Model model, @RequestParam(value="name", required=true) String thename, @RequestParam(value="color", required=true) String thecolor) {
+	
+		model.addAttribute("user", new User());
+		model.addAttribute("products", productService.getProducts());
+ 	
+      return "index";
+    } 
     
    
    
    @PostMapping("/adduser")
     public String addUser(@ModelAttribute User user, Model model) {
     	 
-	// Create Span
-	   Span span = s_tracer.spanBuilder("usingRequestParam").startSpan();
-	   	// Put the span into the current Context
-	   try (Scope scope = span.makeCurrent()) {
-	     
-		 // Set Name tag: This will be our unique way to search for a trace, by specific user at a specifc time in UI.
-			span.setAttribute("name",user.getName());
-		// Set Favorite Color tag: This will allow us to see traffic by "favcolor" in UI.
-			span.setAttribute("favcolor", user.getColor());
         // ORIGINAL CODE
 			model.addAttribute("products", productService.getProducts());
 		// END ORIGINAL CODE
-	   
-	        
-		} finally {
-	          span.end(); 
-	   	}
-
-    	  return "index";
+		return "index";
     }
  
 }
